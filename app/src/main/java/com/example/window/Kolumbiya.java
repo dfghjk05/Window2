a43df8d48ce3f0400be6a37cc385785ef925e440
Alexandr <alexandr@MacBook-Pro-Alexandr.local>
2024-02-09 10:30:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/window/Kolumbiya.java
@@ -0,0 +1,14 @@
+package com.example.window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kolumbiya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kolumbiya);
+    }
+}
